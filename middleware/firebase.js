a450089a829033dc2b4e0ca923a12f2cddcb8bd1
Uriel Ofir <urielofir@gmail.com>
2021-10-14 16:54:43 +0300

    adding firebase

---
+++ middleware/firebase.js
@@ -0,0 +1,36 @@
+import { initializeApp } from 'firebase/app';
+import {  getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+
+
+export default async function googleAuth() {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            return user;
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        });
+};
